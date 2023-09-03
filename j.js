b20ab15cd1184c707020194e2e9a1b5507815fc4
DanyWarrior <danyprogramando@gmail.com>
2023-09-03 13:01:01 -0400

    Creating divs with javascript and adding them on the HTML document with an array of objects

---
+++ j.js
@@ -0,0 +1,237 @@
+
+ 
+
+let albums = [
+    {
+        title: "Thriller", 
+        artist: "Michael Jackson", 
+        year: 1982, 
+        genre: "Pop"},
+
+    {
+        title: "Purple Rain", 
+        artist: "Prince and The Revolution", 
+        year: 1984, 
+        genre: "Pop/Rock"},
+
+    {
+        title: "Born in the U.S.A.", 
+        artist: "Bruce Springsteen", 
+        year: 1984, 
+        genre: "Rock"},
+
+    {
+        title: "Synchronicity", 
+        artist: "The Police", 
+        year: 1983, 
+        genre: "New Wave"},
+
+    {
+        title: "Appetite for Destruction", 
+        artist: "Guns N' Roses", 
+        year: 1987, 
+        genre: "Hard Rock"},
+
+    {
+        title: "Like a Virgin", 
+        artist: "Madonna", 
+        year: 1984, 
+        genre: "Pop"},
+
+    {
+        title: "The Joshua Tree", 
+        artist: "U2", 
+        year: 1987, 
+        genre: "Rock"},
+
+    {
+        title: "Graceland", 
+        artist: "Paul Simon", 
+        year: 1986, 
+        genre: "World Music"},
+
+    {
+        title: "Rio", 
+        artist: "Duran Duran", 
+        year: 1982, 
+        genre: "New Wave"},
+
+    {
+        title: "Brothers in Arms", 
+        artist: "Dire Straits", 
+        year: 1985, 
+        genre:"Rock"
+    }
+];
+
+
+// function countDown(seconds) {
+//     //let seconds = 3
+
+//     let div_countDown = document.createElement("div")
+//     div_countDown.classList.add("div_countDown")
+//     document.body.appendChild(div_countDown)
+
+//     let h3 = document.createElement("h2")
+//     h3.textContent = seconds
+//     div_countDown.appendChild(h3)
+
+//     let countDown = setInterval(() => {
+//         console.log(seconds)
+//         seconds--
+//         h3.textContent = seconds
+//         if (seconds < 0){
+//             clearInterval(countDown)
+//             document.body.removeChild(div_countDown)
+//             document.body.removeChild(div_message)
+//         }
+//     }, 1000);
+//  }
+
+
+let box_container = document.querySelector(".box-container")
+console.log(box_container)
+const btns = document.querySelectorAll(".btn")
+
+
+
+btns.forEach((element) => {
+    
+    element.addEventListener("click", ()=>{
+
+        if(element.textContent === "Yes!"){
+            let div_message = document.createElement("div")
+            div_message.classList.add("div_message")
+            document.body.appendChild(div_message)
+
+            let p = document.createElement("p")
+
+            p.classList.add("p_message")
+            p.textContent = "Awesome, you like good music"
+            div_message.appendChild(p)
+
+
+        //    countDown
+            let seconds = 3
+
+            // creating a div for the Count Down
+            let div_countDown = document.createElement("div")
+            div_countDown.classList.add("div_countDown")
+            document.body.appendChild(div_countDown)
+        
+            // this goes inside the Count Down div to show the numbers
+            let h3 = document.createElement("h3")
+            h3.textContent = seconds
+            div_countDown.appendChild(h3)
+        
+            // running timer
+            let countDown = setInterval(() => {
+                seconds--
+                h3.textContent = seconds
+                if (seconds < 0){
+                    clearInterval(countDown)
+                    document.body.removeChild(div_countDown)
+                    document.body.removeChild(div_message)
+
+                    // removing div class="box-container"
+                    document.body.removeChild(box_container)
+                    
+                    // creating a div to show the Albums
+                    let divData = document.createElement("div")
+                    divData.classList.add("divData")
+                    document.body.appendChild(divData)
+
+                    // creating some kind of table
+                    for (element of albums){
+                        
+                        let pData = document.createElement("p")
+                        pData.textContent = `Name: ${element.title} --- Artist: ${element.artist} ... Year: ${element.year} ... Genre: ${element.genre} `
+                        pData.classList.add("pData")
+                        divData.appendChild(pData)
+                        console.log(element.title)
+                    }
+                }
+            }, 1000);
+        } 
+
+        else {
+            let div_message = document.createElement("div")
+            div_message.classList.add("div_message")
+            document.body.appendChild(div_message)
+
+            let p = document.createElement("p")
+
+            p.classList.add("p_message")
+            p.textContent = "That's a Shame, you are missing out  my friend"
+            div_message.appendChild(p)
+
+            
+
+
+            //    countDown
+            let seconds = 3
+
+            let div_countDown = document.createElement("div")
+            div_countDown.classList.add("div_countDown")
+            document.body.appendChild(div_countDown)
+        
+            let h3 = document.createElement("h3")
+            h3.textContent = seconds
+            div_countDown.appendChild(h3)
+        
+            let countDown = setInterval(() => {
+                console.log(seconds)
+                seconds--
+                h3.textContent = seconds
+                if (seconds < 0){
+                    clearInterval(countDown)
+                    document.body.removeChild(div_countDown)
+                    document.body.removeChild(div_message)
+                }
+            }, 1000);
+        }
+    })
+
+    }
+);
+
+
+
+
+
+// // testing
+// let personas = 
+// [
+//     {
+//     name: "Dany",
+//     apellido: "Fojo"
+//     },
+
+//     {
+//     name: "Nana",
+//     apellido: "Bernal"
+//     }
+// ]
+    
+
+// personas.forEach((element) => {
+//     console.log(element.name)
+// })
+
+
+// let a = document.getElementById("name")
+// let aa = document.getElementById("apelli")
+// let b = document.getElementById("b")
+
+// b.addEventListener("click", ()=>{
+//     const obj = {
+//         name: a.value,
+//         appellido: aa.value
+//     }
+//     personas.push(obj)
+//     console.log(personas)
+
+//     let divData = document.createElement("div")
+//     divData.classList.add("divData")
+//     document.body.appendChild(divData)
+// })
